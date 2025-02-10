9a5ab19cb3920e5648810337733cc3d6f17c775b
tvtn24 <122183965+tvtn24@users.noreply.github.com>
2025-02-10 16:49:59 +0000

    add color

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    theme: {
+      colors: {
+        'green' : '#1DB954',
+        'dark-green' : '#168d40',
+        'light-green' : '#2bde6a',
+        'black' : '#191414'
+      }
+    }
+  }
+
