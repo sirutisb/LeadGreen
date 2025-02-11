c2b90a92e64fa1b25c0cdf5873d4bff2c3a5668e
tvtn24 <122183965+tvtn24@users.noreply.github.com>
2025-02-11 15:22:14 +0000

    create section components

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import { ArrowRight, Send, Menu, TreePine } from "lucide-react"
 import FeatureCard from "../components/FeatureCard"
 import NavBar from "../Components/Navbar"
+import Section from "../Components/Section"
 
 export default function Home() {
   return (
@@ -8,19 +9,7 @@ export default function Home() {
       <NavBar></NavBar>
 
       <main className="container mx-auto px-4 py-16">
-        <section className="text-center mb-16">
-          <h1 className="text-4xl md:text-6xl font-bold text-green-700 mb-4">Track Your Sustainability Journey</h1>
-          <p className="text-xl text-green-700 mb-8">
-            Empower yourself to make eco-friendly choices and reduce your carbon footprint.
-          </p>
-          <a
-            href="#get-started"
-            className="inline-flex items-center px-6 py-3 text-lg font-semibold text-white bg-green-600 rounded-full hover:bg-green-700 transition-colors duration-300"
-          >
-            Get Started
-            <ArrowRight className="ml-2 h-5 w-5" />
-          </a>
-        </section>
+        <Section></Section>
         
         <section id="features" className="grid md:grid-cols-3 gap-8 mb-16">
           <button 
