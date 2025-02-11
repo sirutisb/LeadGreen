694ec95f81a52f3348ba5c23cce8b93d4873e205
tvtn24 <122183965+tvtn24@users.noreply.github.com>
2025-02-11 15:12:01 +0000

    update usernav

--- Frontend/src/Components/NavBar/UserNav.jsx
+++ Frontend/src/Components/NavBar/UserNav.jsx
@@ -1,18 +1,18 @@
 import { useState } from "react";
-import { User, AlignJustify } from 'lucide-react';
+import { User, AlignJustify } from "lucide-react";
 
-export default function UserNav(){
-    const [isOpen, setIsOpen] = useState(false);
-    return(
-        <div className="p-2 inline-block border rounded-full text-green-700 h-14 w-20 items-center">
-            <button 
-                onClick={() => setIsOpen(!isOpen)}
-                className="flex justify-between items-center hover:opacity-80  transition cursor-pointer ">
-                
-                <AlignJustify/>
-                <User />
+export default function UserNav() {
+  const [isOpen, setIsOpen] = useState(false);
 
-            </button>
-        </div>
-    )
+  return (
+    <div className="flex items-center justify-center p-2 border rounded-full text-green-700 h-14 w-22 transition cursor-pointer">
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className="w-full h-full flex items-center justify-center gap-2 hover:opacity-80 transition cursor-pointer"
+      >
+        <AlignJustify />
+        <User />
+      </button>
+    </div>
+  );
 }
