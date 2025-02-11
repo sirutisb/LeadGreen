cf3817133ce592c177f6965cea0ab57da4e88045
tvtn24 <122183965+tvtn24@users.noreply.github.com>
2025-02-11 15:05:29 +0000

    m

--- Frontend/src/Components/NavBar/UserNav.jsx
+++ Frontend/src/Components/NavBar/UserNav.jsx
@@ -4,10 +4,10 @@ import { User, AlignJustify } from 'lucide-react';
 export default function UserNav(){
     const [isOpen, setIsOpen] = useState(false);
     return(
-        <div className="p-2 inline-block border rounded-full text-green-700 h-12 w-18 items-center">
+        <div className="p-2 inline-block border rounded-full text-green-700 h-14 w-20 items-center">
             <button 
                 onClick={() => setIsOpen(!isOpen)}
-                className="flex justify-between items-center space-8">
+                className="flex justify-between items-center hover:opacity-80  transition cursor-pointer ">
                 
                 <AlignJustify/>
                 <User />
