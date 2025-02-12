9375f4efc62227d7e9251052578869e4f04f05d2
sirutisb <87762791+sirutisb@users.noreply.github.com>
2025-02-12 18:15:08 +0000

    delete broken bugged 'frontend' folder

--- frontend/src/Hooks/MockData.js
+++
@@ -1,8 +0,0 @@
-const mockPosts = Array.from({ length: 50 }, (_, i) => ({
-    id: i + 1,
-    title: `Post Title ${i + 1}`,
-    body: `This is the body of post ${i + 1}. More content goes here.`,
-  }));
-  
-  export default mockPosts;
-  
