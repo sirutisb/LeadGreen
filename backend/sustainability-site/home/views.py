7f9295cd9a31597f66183e8e277175bc6d1518a0
Benitas <benitas987@gmail.com>
2025-02-12 17:49:22 +0000

    update home page

--- backend/sustainability-site/home/views.py
+++ backend/sustainability-site/home/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Example homepage")
+    return HttpResponse("<h1>Welcome to LeadGreen</h1>")
 
 def posts(request):
     return HttpResponse("Welcome to the posts, this is a collection of the public posts.") 
