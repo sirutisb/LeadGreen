2e0e5b9365edbae9e5847a2e7602e8b3574a07e4
Benitas <benitas987@gmail.com>
2025-02-12 17:37:22 +0000

    fixed runtime error

--- backend/sustainability-site/home/urls.py
+++ backend/sustainability-site/home/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views, include
+from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('home/', views.index, name='index'),
     path('posts/', views.posts)
 ]
