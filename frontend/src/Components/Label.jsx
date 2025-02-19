22c1b3388536f751300543c7230fb78c81ff8014
tvtn24 <122183965+tvtn24@users.noreply.github.com>
2025-02-19 17:12:23 +0000

    validating register form
    change to input and label cuz these props not helping dawg

--- frontend/src/Components/Label.jsx
+++ frontend/src/Components/Label.jsx
@@ -1,9 +1,14 @@
+
 const Label = ({ children, htmlFor, className = "", ...props }) => {
-    return (
-      <label htmlFor={htmlFor} className={`block text-sm font-medium text-gray-700 ${className}`} {...props}>
-        {children}
-      </label>
-    )
-  }
-  
-  export default Label
+  return (
+    <label
+      htmlFor={htmlFor}
+      className={`block text-sm font-medium text-gray-700 ${className}`}
+      {...props}
+    >
+      {children}
+    </label>
+  );
+};
+
+export default Label;
