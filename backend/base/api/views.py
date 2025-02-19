38131f90ad3fd5848fe8b93508f39195c2b011cc
sirutisb <87762791+sirutisb@users.noreply.github.com>
2025-02-19 20:57:19 +0000

    fix naming for consistency

--- backend/base/api/views.py
+++ backend/base/api/views.py
@@ -24,8 +24,8 @@ class RegisterView(APIView):
             refresh = RefreshToken.for_user(user)
             return Response({
                 "message": "User registered successfully!",
-                "access_token": str(refresh.access_token),
-                "refresh_token": str(refresh),
+                "access": str(refresh.access_token),
+                "refresh": str(refresh),
             }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
