57f870fb70fa9107503aab68db69990363ceb460
duc-minh-droid <ducminhcsp@gmail.com>
2025-02-20 12:45:21 +0000

    fixed ts

--- frontend/src/Components/NavBar/UserNav.jsx
+++ frontend/src/Components/NavBar/UserNav.jsx
@@ -1,64 +1,27 @@
-import { useState } from "react";
-import { User, AlignJustify } from "lucide-react";
-import {useNavigate} from "react-router-dom";
-import Popup from 'reactjs-popup';
-import 'reactjs-popup/dist/index.css';
-import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { User } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import AuthContext from "../../Context/AuthContext";
 
 export default function UserNav() {
-  const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
+  const { user, logoutUser } = useContext(AuthContext);
 
-  const handleLogout = () => {
-    //Logout logic 
-    localStorage.removeItem("authToken");
-    
-    // Navigate to the login page after logout
-    navigate("/login");
+  const handleAuthAction = () => {
+    if (user) {
+      logoutUser(); // If logged in, log out
+    } else {
+      navigate("/login"); // If not logged in, navigate to login
+    }
   };
 
   return (
-    <div className="flex items-center justify-between border rounded-full bg-green-700 h-14 w-22 transition cursor-pointer hover:bg-green-600">
-      <Popup
-        trigger={
-          <button
-          onClick={() => setIsOpen(!isOpen)}
-          className="w-full h-full flex items-center justify-center gap-2 transition cursor-pointer "
-        >
-          <AlignJustify />
-          <User />
-        </button>
-        }
-        position="bottom right"
-        closeOnDocumentClick
-        arrow={false}
-        contentStyle={{ padding: "0", border: "none" , margin: "4px"}}
-      >
-        <div className="flex flex-col bg-white rounded-xl shadow-lg">
-          <Link
-            to="/login"
-            className="px-4 py-2 text-green-700 hover:bg-green-100"
-          >
-            Sign In
-          </Link>
-          <Link
-            to="/register"
-            className="px-4 py-2 text-green-700 hover:bg-green-100"
-          >
-            Register
-          </Link>
-
-          <button 
-              onClick={handleLogout} 
-              className="px-4 py-2 text-red-700 hover:bg-green-100 "
-            >
-              Log Out
-          </button>
-
-        </div>
-      </Popup>
-    </div>
+    <button
+      onClick={handleAuthAction}
+      className="flex items-center gap-2 bg-green-700 hover:bg-green-600 text-white font-semibold px-4 py-2 rounded-full transition"
+    >
+      <User />
+      {user ? "Logout" : "Login"}
+    </button>
   );
 }
-
-
