46bba9c39cfc87c01e44b7d30c54ec14f57d6290
Taha-Mukati <160735849+Taha-Mukati@users.noreply.github.com>
2025-02-20 17:06:49 +0000

    Views, urls and serializers for Leaderboard

--- backend/leaderboard/views.py
+++ backend/leaderboard/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import generics
+from users.models import UserProfile
+from .serializers import UserProfileSerializer
 
-# Create your views here.
+class list_pointsBalance(generics.ListAPIView):
+    queryset = UserProfile.objects.all().order_by('-points_balance')
+    serializer_class = UserProfileSerializer
+
+class list_lifetimePoints(generics.ListAPIView):
+    queryset = UserProfile.objects.all().order_by('-lifetime_points')
+    serializer_class = UserProfileSerializer
+
+class list_treeLevel(generics.ListAPIView):
+    queryset = UserProfile.objects.all().order_by('-tree_level')
+    serializer_class = UserProfileSerializer
+
