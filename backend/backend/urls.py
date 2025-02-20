7f344ca53256628f545a18d2b504c932b85ccf3a
sirutisb <87762791+sirutisb@users.noreply.github.com>
2025-02-20 23:34:56 +0000

    media urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
@@ -26,3 +29,6 @@ urlpatterns = [
     path('api/qrcodes/', include('qrcodes.urls')),
     path('api/users/', include('users.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
