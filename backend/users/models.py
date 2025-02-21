d15b2322f83075af59baa121c1697c2b1490d38c
bananaboy10 <ephgravejed@gmail.com>
2025-02-21 17:18:12 +0000

    Seperated game field from user profile

--- backend/users/models.py
+++ backend/users/models.py
@@ -13,9 +13,24 @@ class UserProfile(AbstractUser):
     
     points_balance = models.IntegerField(default=0)
     lifetime_points = models.IntegerField(default=0)
+
+    ''' 
+    uncomment if changing method of seperating base profile / game, etc...
+
     tree_level = models.IntegerField(default=1) # Current tree level
     tree_growth = models.FloatField(default=0.0) # Progress towards next level
     has_snail = models.BooleanField(default=False)
+    '''
 
     def __str__(self):
         return f"{self.username} - Points: {self.points_balance} | Level: {self.tree_level}"
+    
+
+class UserGameProfile(models.Model):
+    # relate to user profile
+    user = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+    
+    # user fields only relevant to game profile
+    tree_level = models.IntegerField(default=1) # current tree level
+    tree_growth = models.FloatField(default=0.0) # Progress towards next level
+    has_snail = models.BooleanField(default=False)
