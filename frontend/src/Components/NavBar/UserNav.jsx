e777105c93e3ffe30aca4142c58f80eb23f42533
tvtn24 <122183965+tvtn24@users.noreply.github.com>
2025-02-21 13:57:59 +0000

    change circle, add animation to navbar

--- frontend/src/Components/NavBar/UserNav.jsx
+++ frontend/src/Components/NavBar/UserNav.jsx
@@ -19,7 +19,7 @@ export default function UserNav() {
   return (
     <motion.button
       onClick={handleAuthAction}
-      className={`flex items-center gap-3 px-6 py-3 text-white font-semibold rounded-full shadow-lg transition-all duration-200
+      className={`flex items-center gap-3 px-6 py-3 text-white font-semibold rounded-full shadow-lg transition-all duration-200 cursor-pointer
         ${user ? "bg-red-600 hover:bg-red-700" : "bg-green-700 hover:bg-green-600"}
       `}
     >
