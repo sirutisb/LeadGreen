58aa257393bd842288dbe3665a7e123c7160eaa2
sirutisb <87762791+sirutisb@users.noreply.github.com>
2025-02-21 01:43:09 +0000

    added consistency to authentication

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -4,6 +4,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
 from authentication.serializers import RegisterSerializer
+from users.serializers import BasicUserProfileSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 
 class RegisterView(APIView):
     def post(self, request):
@@ -12,8 +15,36 @@ class RegisterView(APIView):
             user = serializer.save()
             refresh = RefreshToken.for_user(user)
             return Response({
-                "message": "User registered successfully!",
-                "access": str(refresh.access_token),
-                "refresh": str(refresh),
+                "data": {
+                    "tokens": {
+                        "refresh": str(refresh),
+                        "access": str(refresh.access_token),
+                    },
+                    "user": BasicUserProfileSerializer(user).data
+                }
             }, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        return Response({
+            "errors": serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        try:
+            serializer.is_valid()
+            tokens = serializer.validated_data
+            user = serializer.user
+            return Response({
+                "data": {
+                    "tokens": tokens,
+                    "user": BasicUserProfileSerializer(user).data
+                }
+            }, status=status.HTTP_200_OK)
+            
+        except Exception:
+            return Response({
+                "errors": {
+                    "detail": "Invalid credentials"
+                }
+            }, status=status.HTTP_401_UNAUTHORIZED)
