b5658f3929588d4cc77d871f9a2867fbfab13584
sirutisb <87762791+sirutisb@users.noreply.github.com>
2025-02-22 21:05:48 +0000

    rm line

--- backend/game/urls.py
+++ backend/game/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('tree/water/', views.WaterTreeAction.as_view()),
     path('tree/soil/', views.SoilTreeAction.as_view()),
     path('tree/glove/', views.GloveTreeAction.as_view()),
-    
 ]
