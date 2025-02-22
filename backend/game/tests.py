b5448d5b582ff99e4e057d1c808c24cb23ceef6c
Ralph <sssspppuud@gmail.com>
2025-02-22 15:18:31 +0000

    Laid out tests for game models

--- backend/game/tests.py
+++ backend/game/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from game.models import ShopItem, UserItem, GameState
+from datetime import datetime
 
 # Create your tests here.
+
+class ShopItemModelTest(TestCase):
+    pass
+
+class UserItemModelTest(TestCase):
+    pass
+
+class GameStateModelTest(TestCase):
+    pass
