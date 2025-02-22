7882c98cb015e49b37d3a2c5bfc46638a560cfa4
sirutisb <87762791+sirutisb@users.noreply.github.com>
2025-02-22 19:07:17 +0000

    saved user data to database + added snail logic

--- backend/game/views.py
+++ backend/game/views.py
@@ -13,59 +13,110 @@ from .serializers import ShopItemSerializer, UserItemSerializer
 from rest_framework import generics
 from django.db import models
 
-# Create your views here.
-    
+import random
 
-class WaterTreeAction(APIView):
+# Create your views here.
+
+class TreeGrowAction(APIView):
     permission_classes = [IsAuthenticated]
+    action_cost = 0
+    growth_amount = 0
+    snail_chance = 0
 
     def post(self, request, *args, **kwargs):
         user = request.user
+        
+        if user.has_snail:
+            return Response({
+                "success": False,
+                "message": "There is a snail on the tree! Remove it first.",
+                "points_balance": user.points_balance,
+                "tree_level": user.tree_level,
+                "has_snail": user.has_snail,
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        # adjust level and points
+        if user.points_balance < self.action_cost:
+            return Response({
+                "success": False,
+                "message": f"Not enough points, you have only {user.points_balance}, you need {self.action_cost}",
+                "points_balance": user.points_balance,
+                "tree_level": user.tree_level,
+                "has_snail": user.has_snail,
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        user.tree_growth += self.growth_amount
+        while user.tree_growth >= 1.0:
+            user.tree_growth -= 1.0
+            user.tree_level += 1
+        user.points_balance -= self.action_cost
+
+        # chance for snail to spawn
+        # TODO: make it so theres a cooldown and you cant get the snail again for x amount of actions
+        if random.random() < self.snail_chance:
+            user.has_snail = True
+
+        user.save()
+
+        return Response({
+            "success": True,
+            "message": "action applied successfully",
+            "points_balance": user.points_balance,
+            "tree_level": user.tree_level,
+            "has_snail": user.has_snail,
+        }, status=status.HTTP_200_OK)
+
+
+class WaterTreeAction(TreeGrowAction):
+    action_cost = 10
+    growth_amount = 0.1
+    snail_chance = 0.45
+
+class SoilTreeAction(TreeGrowAction):
+    action_cost = 20
+    growth_amount = 0.3
+    snail_chance = 0.15
+
+
+# TODO: Make cleaner by inheriting
+class GloveTreeAction(APIView):
+    action_cost = 50
+    growth_amount = 0.0
+    permission_classes = [IsAuthenticated]
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        
+        if not user.has_snail:
+            return Response({
+                "success": False,
+                "message": "There is no snail to remove!",
+                "points_balance": user.points_balance,
+                "tree_level": user.tree_level,
+                "has_snail": user.has_snail,
+            }, status=status.HTTP_400_BAD_REQUEST)
         
         # adjust level and points
-        user.tree_level += 0.1
-        user.points_balance -= 10
+        if (user.points_balance < self.action_cost):
+            return Response({
+                "success": False,
+                "message": f"Not enough points, you have only {user.points_balance}, you need {self.action_cost}",
+                "points_balance": user.points_balance,
+                "tree_level": user.tree_level,
+                "has_snail": user.has_snail,
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        user.has_snail = False
+        user.tree_growth += self.growth_amount
+        while user.tree_growth >= 1.0:
+            user.tree_growth -= 1.0
+            user.tree_level += 1
+        user.points_balance -= self.action_cost
+        user.save()
 
         return Response({
             "success": True,
-            "message": "Water applied successfully",
+            "message": "action applied successfully",
             "points_balance": user.points_balance,
             "tree_level": user.tree_level,
-            "has_snail": False,
-        })
-    
-class SoilTreeAction(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request, *args, **kwargs):
-        user = request.user
-        
-        user.tree_level += 0.3
-        user.points_balance -= 20
-
-        return Response({
-            "success": True,
-            "message": "Soil applied successfully",
-            "points_balance": user.points_balance,
-            "tree_level": user.tree_level,
-            "has_snail": False,
-        })
-
-class GloveTreeAction(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request, *args, **kwargs):
-        user = request.user
-        
-        user.tree_level += 0
-        user.points_balance -= 50
-        
-        return Response({
-            "success": True,
-            "message": "Glove applied successfully",
-            "points_balance": user.points_balance,
-            "tree_level": user.tree_level,
-            "has_snail": False,
-        })
-
-
+            "has_snail": user.has_snail,
+        }, status=status.HTTP_200_OK)
