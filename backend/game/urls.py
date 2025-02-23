29f38732e7dd084177efe2b40a025dd8f269681b
bananaboy10 <ephgravejed@gmail.com>
2025-02-23 11:30:13 +0000

    Url for gameprofile get request

--- backend/game/urls.py
+++ backend/game/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('tree/water/', views.WaterTreeAction.as_view()),
     path('tree/soil/', views.SoilTreeAction.as_view()),
     path('tree/glove/', views.GloveTreeAction.as_view()),
+    path('gameprofile/', views.GameProfileView.as_view()),
 ]
