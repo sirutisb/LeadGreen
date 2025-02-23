5ece874adcf4d68189704a5bf153908c2dd0f856
sirutisb <87762791+sirutisb@users.noreply.github.com>
2025-02-23 01:01:03 +0000

    register other game models in database

--- backend/game/admin.py
+++ backend/game/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-# from .models import
-from .models import ShopItem, UserItem
+from .models import ShopItem, UserItem, GameProfile, Plant, Insect
 
 # Register your models here.
 
 admin.site.register(ShopItem)
 admin.site.register(UserItem)
+
+admin.site.register(GameProfile)
+
+admin.site.register(Plant)
+admin.site.register(Insect)
