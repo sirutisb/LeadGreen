42875e85554e042c05781bb010aaeb2a1253769d
Taha-Mukati <160735849+Taha-Mukati@users.noreply.github.com>
2025-02-24 16:32:56 +0000

    push

--- backend/leaderboard/serializers.py
+++ backend/leaderboard/serializers.py
@@ -2,10 +2,6 @@ from django.utils import timezone
 from rest_framework import serializers
 from game.models import GameProfile
 
-<<<<<<< HEAD
-# creating serializer for gameprofiles in leaderboard
-=======
->>>>>>> 7a3116bebb5dff8fe0dc8ffccb642df6fd33a666
 class GameProfileLeaderboardSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='user.username', read_only=True)  # Get the username from the related UserProfile model
 
