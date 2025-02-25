1be938ad17d50e37b029bb9504ba58c3b2c1bbc5
bananaboy10 <ephgravejed@gmail.com>
2025-02-25 01:08:33 +0000

    test info

--- backend/game/tests.py
+++ backend/game/tests.py
@@ -1,17 +1,17 @@
 from django.test import TestCase
-from django.contrib.auth import get_user_model
 from users.models import UserProfile
-from rest_framework.test import APITestCase
-from rest_framework import status
-from .models import GameProfile
+
 from django.urls import reverse
 
 from django.test import TestCase
+from .models import Plant, Insect, GameProfile
 from users.models import UserProfile
-from .models import Plant, Insect
 
+# for testing API calls in views later
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-
+"""
 class ShopItemTestCase(TestCase):
 
     # add shop item to DB - test if DB stores expected results
@@ -49,4 +49,20 @@ class UserItemTestCase(TestCase):
 
         self.assertEqual(user_item.quantity, 1)
         self.assertEqual(str(user_item), "testname - x1 item1")
+"""
 
+class GameModelTest(TestCase):
+    def setUp(self):
+        self.user = UserProfile.objects.create(username='testuser')
+        self.plant1 = Plant.objects.create(name='Leafy', level=1)
+        self.plant2 = Plant.objects.create(name='Sprouto', level=2)
+        self.insect1 = Insect.objects.create(name='Buzzly', level=1, spawn_chance=0.5)
+        self.insect2 = Insect.objects.create(name='Creepsy', level=2, spawn_chance=0.3)
+        self.game_profile = GameProfile.objects.get(user=self.user) 
+
+    def test_create_game_profile(self):
+        self.assertIsNotNone(self.game_profile)
+        self.assertEqual(self.game_profile.user, self.user)
+        self.assertEqual(self.game_profile.points_balance, 100)
+        self.assertEqual(self.game_profile.tree_level, 1)
+        self.assertEqual(self.game_profile.tree_growth, 0.0)
