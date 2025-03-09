97325744aab8c24acf53d42bd85d7259ff2bd4b5
sirutisb <87762791+sirutisb@users.noreply.github.com>
2025-03-09 22:50:47 +0000

    Update serializers.py

--- backend/leaderboard/serializers.py
+++ backend/leaderboard/serializers.py
@@ -2,13 +2,17 @@ from django.utils import timezone
 from rest_framework import serializers
 from game.models import GameProfile
 
+from users.models import UserProfile
+from users.serializers import BasicUserProfileSerializer
+
 class GameProfileLeaderboardSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(source='user.username', read_only=True)  # Get the username from the related UserProfile model
-    user_image = serializers.ImageField(source='user.profile_picture', read_only=True)
+    user = BasicUserProfileSerializer(read_only=True)
+    #username = serializers.CharField(source='user.username', read_only=True)  # Get the username from the related UserProfile model
+    #user_image = serializers.ImageField(source='user.profile_picture', read_only=True)
 
     class Meta:
         model = GameProfile
         fields = [
-            'user_image' ,'username', 'points_balance', 'lifetime_points', 'tree_level',
+            'user', 'points_balance', 'lifetime_points', 'tree_level',
         ]
-        read_only_fields = ['user_image', 'username', 'points_balance', 'lifetime_points', 'tree_level',]
+        read_only_fields = ['user', 'points_balance', 'lifetime_points', 'tree_level',]
