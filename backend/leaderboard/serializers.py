b462589df5f4c2c31bf44526917b693a5c244072
Taha-Mukati <160735849+Taha-Mukati@users.noreply.github.com>
2025-03-09 19:23:48 +0000

    push

--- backend/leaderboard/serializers.py
+++ backend/leaderboard/serializers.py
@@ -4,10 +4,11 @@ from game.models import GameProfile
 
 class GameProfileLeaderboardSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='user.username', read_only=True)  # Get the username from the related UserProfile model
+    user_image = serializers.ImageField(source='user.profile_picture', read_only=True)
 
     class Meta:
         model = GameProfile
         fields = [
-            'username', 'points_balance', 'lifetime_points', 'tree_level', 
+            'user_image' ,'username', 'points_balance', 'lifetime_points', 'tree_level',
         ]
-        read_only_fields = ['username', 'points_balance', 'lifetime_points', 'tree_level',]
+        read_only_fields = ['user_image', 'username', 'points_balance', 'lifetime_points', 'tree_level',]
