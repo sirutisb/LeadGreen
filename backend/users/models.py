446a8e996adfe1dbb9e52681daedf705e662fc50
sirutisb <87762791+sirutisb@users.noreply.github.com>
2025-03-09 00:03:35 +0000

    remove commented code

--- backend/users/models.py
+++ backend/users/models.py
@@ -9,14 +9,7 @@ class UserProfile(AbstractUser):
         default='profile_pics/default.png',
         blank=True,
         null=True
-        )
-
-    # Dont use anymore, instead use GameProfile model
-    # points_balance = models.IntegerField(default=0)
-    # lifetime_points = models.IntegerField(default=0)
-    # tree_level = models.IntegerField(default=1) # Current tree level
-    # tree_growth = models.FloatField(default=0.0) # Progress towards next level
-    # has_snail = models.BooleanField(default=False)
+    )
 
     def __str__(self):
         return f"{self.username}"
