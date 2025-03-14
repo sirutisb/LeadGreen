d5729de6ba3667c22563bd25e93166e8dbde20f7
Taha-Mukati <160735849+Taha-Mukati@users.noreply.github.com>
2025-03-14 16:54:29 +0000

    profile page

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import UserProfile
+from posts.models import Post
 
 # For use in posts and profile pages etc
 class BasicUserProfileSerializer(serializers.ModelSerializer):
@@ -19,26 +20,22 @@ class UserWithGameSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'profile_picture', 'points_balance', 'tree_level', 'current_plant']
 
 class UserPageSerializer(serializers.ModelSerializer):
-    # Game-related fields from GameProfi
+    points_balance = serializers.IntegerField(source='game_profile.points_balance')
     class Meta:
         model = UserProfile
-        fields = ['id', 'username', 'profile_picture', 'date_joined', 'points_balance']
-
+        fields = ['id', 'username', 'profile_picture', 'points_balance']
 
 class UserWithPostSerializer(serializers.ModelSerializer):
     # Post-related fields from Post
-    post_count = serializers.IntegerField(source='posts.count')
-    latest_post = serializers.SerializerMethodField()
 
     class Meta:
-        model = UserProfile
-        fields = ['id', 'username', 'profile_picture', 'post_count', 'latest_post', 'points_balance', 'tree_level']
+        model = Post
+        fields = ['caption', 'created_at', 'points_received']
 
     def get_latest_post(self, obj):
-        post = obj.posts.order_by('-created_at').first()
+        post = obj.post_set.order_by('-created_at').first()
         if post:
             return {
-                'profile_picture': post.user.profile_picture.url if post.user.profile_picture else None,
                 'caption': post.caption,
                 'created_at': post.created_at,
                 'points' : post.points_received
--- backend/users/views.py
+++ backend/users/views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from .models import UserProfile
 from game.serializers import PlantProgressSerializer, InsectSerializer
-from .serializers import  BasicUserProfileSerializer, UserWithGameSerializer, UserWithPostSerializer
+from .serializers import  BasicUserProfileSerializer, UserWithGameSerializer, UserWithPostSerializer, UserPageSerializer
 from game.models import GameProfile
 
 
@@ -17,14 +17,14 @@ from posts.pagination import PostPagination
 from posts.models import Post
 from rest_framework import generics
 
-class UserProfileView(APIView):
-    def get(self, request, pk):
-        try:
-            user = UserProfile.objects.get(id=pk)
-            serializer = UserWithGameSerializer(user, many=False)
-            return Response(serializer.data)
-        except UserProfile.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+# class UserProfileView(APIView):
+#     def get(self, request, pk):
+#         try:
+#             user = UserProfile.objects.get(id=pk)
+#             serializer = UserWithGameSerializer(user, many=False)
+#             return Response(serializer.data)
+#         except UserProfile.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
 
 class UserPostsView(generics.ListAPIView):
     serializer_class = PostSerializer
@@ -34,7 +34,7 @@ class UserPostsView(generics.ListAPIView):
         user_id = self.kwargs['id']
         return Post.objects.filter(user_id=user_id).order_by('-created_at') # Should it only show their approved posts?
     
-class UserPageDetails(APIView):
+class UserProfileView(APIView):
     def get(self, request, pk):
         try:
             user = UserProfile.objects.get(id=pk)
@@ -48,9 +48,9 @@ class UserPageDetails(APIView):
            
 
             return Response({
-                'user' : UserPageDetails(user).data,
+                'user' : UserPageSerializer(user).data,
                 'tree' : PlantProgressSerializer(user.game_profile).data,
-                'posts' : PostSerializer(user.posts.all().order_by('-created_at'), many=True,).data,
+                'posts' : UserWithPostSerializer(user.post_set.all().order_by('-created_at'), many=True,).data,
                 'rank' : rank
             })
         except UserProfile.DoesNotExist:
