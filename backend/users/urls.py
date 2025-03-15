cf5e18fb26ba3e87ecba5a7053786ae879959c53
sirutisb <87762791+sirutisb@users.noreply.github.com>
2025-03-15 21:26:00 +0000

    fix api url

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>', views.UserProfileView.as_view(), name='user-profile'),
+    path('<int:pk>/', views.UserProfileView.as_view(), name='user-profile'),
     path('<int:id>/posts', views.UserPostsView.as_view(), name='user-posts'),
 ]
