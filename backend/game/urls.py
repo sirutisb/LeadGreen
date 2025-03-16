69f9229fe5c612e33b0b252212d6a8ad77948dce
sirutisb <87762791+sirutisb@users.noreply.github.com>
2025-03-16 09:50:26 +0000

    removed unused item code

--- backend/game/urls.py
+++ backend/game/urls.py
@@ -8,9 +8,6 @@ router.register(r'inventory', views.InventoryViewSet, basename='inventory')
 
 urlpatterns = [
     path('', views.GameProfileView.as_view()), # Game Profile Get Request URL (at root)
-    path('tree/water/', views.WaterTreeAction.as_view()),
-    path('tree/soil/', views.SoilTreeAction.as_view()),
-    path('tree/glove/', views.GloveTreeAction.as_view()),
     path('spin/', views.SpinView.as_view()),
     path('spin/prizes/', views.GetPrizes.as_view()),
     path('use-item/<int:item_id>/', views.UseItemView.as_view(), name='use-item'),
