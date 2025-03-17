d6fd6ba6beffc1d48b0b229fba7b40cfcbbda10e
sirutisb <87762791+sirutisb@users.noreply.github.com>
2025-03-17 17:32:07 +0000

    leaderboard pagination

--- backend/leaderboard/views.py
+++ backend/leaderboard/views.py
@@ -7,9 +7,9 @@ from rest_framework.response import Response
 
 #
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 100 # Show all users for now
+    page_size = 10
     page_size_query_param = 'page_size'
-    max_page_size = 100
+    max_page_size = 50
 
 
 class list_pointsBalance(generics.ListAPIView):
