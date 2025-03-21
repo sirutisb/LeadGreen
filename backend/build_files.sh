eae4d93e3d47b568748912542cdb7e12205a01d1
sirutisb <87762791+sirutisb@users.noreply.github.com>
2025-03-21 10:39:58 +0000

    basic build #3

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+echo "Building the project..."
 pip install -r requirements.txt
-python manage.py collectstatic --noinput 
+echo "Collecting static files..."
+#python manage.py collectstatic --noinput 
+
+python manage.py makemigrations
+python manage.py migrate
+
+echo "Build completed successfully!"
